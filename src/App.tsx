c932453e4368d169ad101c8e627eb24ea2b6034b
Dspatel204 <dishant.sureshbhai@gmail.com>
2024-12-01 15:31:53 +0530

    added a website

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <div className="top-bar">
+          First Order Get Free Up to 20% on Our Selected Jewelery.
+        </div>
+        <header>
+          <div className="logo">Kanvica</div>
+          <nav>
+            <ul>
+              <li>
+                <a href="#contact">Contact Us</a>
+              </li>
+              <li>
+                <a href="#products">Shop Products</a>
+              </li>
+              <li>
+                <a href="#accessories">Accessories</a>
+              </li>
+              <li>
+                <a href="#book">Collections</a>
+              </li>
+            </ul>
+          </nav>
+          <button className="btn-appointment">Book an Appointment</button>
+        </header>
+      </div>
+    </>
+  );
+}
+
+export default App;
